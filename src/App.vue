9743ead158564faa928077720778b3a1d7178b41
Yang Qi <60582494+YangQi007@users.noreply.github.com>
2024-03-13 16:06:07 -0600

    Cleand up and restructured code for pakaging

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,22 @@
 <script>
-// import timeline from "./components/timeline.vue"
-// import timeline_d3 from "./components/timeline_d3.vue"
-import vanillaJs_timeline from "./components/vanillaJs_timeline.vue";
-
-
+import Timeline from "./components/Timeline.vue";
 
 export default {
   name: 'App',
   components: {
-    // timeline,
-    // timeline_d3,
-    vanillaJs_timeline
+    Timeline
      
   },
-  data: () => ({
-    
-  }),
-  computed: {
-    
-  },
-  methods: {
-    
-  },
-  created () {
-    
-  }
 }
 
 </script>
 
 <template>
   <v-app>
-      <!-- <timeline></timeline> -->
-      <!-- <timeline_d3></timeline_d3> -->
-      <vanillaJs_timeline></vanillaJs_timeline>
+      <Timeline></Timeline>
   </v-app>
 </template>
 
 <style lang="sass">
 
-</style>
+</style>./components/Timeline.vue
